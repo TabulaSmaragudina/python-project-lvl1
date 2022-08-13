7a0b2d637026769d02e9d6d5a80fa95b5c03a435
TabulaSmaragudina <ilsonkin@yandex.ru>
2022-08-13 13:55:13 +0300

    step 3

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python3
+
+
 def main():
     print('Welcome to the Brain Games!')
+    from brain_games.scripts.cli import welcome_user
+    return welcome_user()
+
 if __name__ == '__main__':
     main()
 
