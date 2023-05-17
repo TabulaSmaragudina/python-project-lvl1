e89a0182e2e0ad606681a06eda90259b6512646c
TabulaSmaragudina <ilsonkin@yandex.ru>
2023-05-17 03:29:58 +0300

    f

--- brain_games/games/even_logic.py
+++ brain_games/games/even_logic.py
@@ -1,7 +1,7 @@
 from random import randint
 
 
-Description = f'Answer "yes" if the number is even, otherwise answer "no".'
+Description = "Answer \"yes\" if the number is even, otherwise answer \"no\"."
 
 
 def run_logic():
